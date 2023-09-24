1d55e1cea3433d2a8f6fce580573f0e3ec50cddc
apoorvapendse <apoorvavpendse@gmail.com>
2023-09-24 13:52:00 +0530

    adds admin if not present in collection

--- quiz-app/src/components/AdminDashboard.jsx
+++ quiz-app/src/components/AdminDashboard.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import firebase from 'firebase/compat/app'
 
 import { initializeApp } from "firebase/app";
-import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { addDoc, collection, getDoc, getDocs, getFirestore, query, where } from "firebase/firestore";
 import { firebaseConfig as conf, app } from '../../firebase';
 
 
@@ -11,17 +11,51 @@ const firestore = getFirestore(app);
 
 const AdminDashboard = ({ admin }) => {
     console.log(admin.email)
-    const addQuiz = () => {
-        addDoc(collection(firestore, "Quizes"), {
-            owner: admin.email,
-            name: "Apoorva",
 
 
-        })
-            .then((doc) => {
-                console.log("Doc created:", doc)
-            })
+    const getAdminID = async () => {
+        const adminRef = collection(firestore, "Admins")
+        const adminQuery = query(adminRef, where("email", "==", `${admin.email}`));
+
+        const queryResults = await getDocs(adminQuery);
+        return queryResults.docs[0].id;
     }
+
+    const addAdmin = async () => {
+
+        const adminRef = collection(firestore, "Admins")
+        const adminQuery = query(adminRef, where("email", "==", `${admin.email}`));
+
+        const queryResults = await getDocs(adminQuery);
+
+        if (queryResults.size == 0) {
+            const newAdmin = await addDoc(collection(firestore, "Admins"), {
+                email: admin.email
+            })
+
+            console.log("Admin created:", newAdmin);
+        }
+        else {
+            console.log("Admin already exists")
+        }
+
+    }
+
+    const addQuiz = async () => {
+
+
+        const quiz = await addDoc(collection(firestore, `Admins/${await getAdminID()}/Quizes`), {
+            owner: admin.email,
+            name: "quizyquiz",
+        })
+        console.log(quiz)
+
+    }
+
+    useEffect(() => {
+        //this will create an admin if he already doesn't exist in the admins collection
+        addAdmin();
+    }, [])
 
 
 
@@ -29,6 +63,7 @@ const AdminDashboard = ({ admin }) => {
         <>
             <h1>AdminDashboard</h1>
             <button onClick={() => addQuiz()}>Create Quiz Doc</button>
+            <button onClick={() => addAdmin()}>Add Admin</button>
         </>
     )
 }
